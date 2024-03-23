c2d546c3afaae2697c98371081ee0c93d4aca941
BizNextStep <charmaine.n.mbatha@icloud.com>
2024-03-23 16:07:54 -0700

    updates

--- models/place.py
+++ models/place.py
@@ -1,32 +1,42 @@
 #!/usr/bin/python3
-"""defines a Place class, a subclass of BaseModel class."""
+
+"""Defines a Place class, a subclass of BaseModel."""
 from models.base_model import BaseModel
+from sqlalchemy import Column, String, Integer, Float, ForeignKey, Table
+from sqlalchemy.orm import relationship
 
 
 class Place(BaseModel):
     """
-    A subclass of BaseModel class
+    A subclass of BaseModel class.
     Public class attributes:
-        city_id:             (str) will be City.id
-        user_id:             (str) will be User.id
-        name:                (str)
-        description:         (str)
-        number_rooms:        (int) 0
-        number_bathrooms:    (int) 0
-        max_guest:           (int) 0
-        price_by_night:      (int) 0
-        latitude:            (float) 0.0
-        longitude:           (float) 0.0
+        __tablename__:       (str) represents the table name, places
+        city_id:             (str) can't be null, foreign key to cities.id
+        user_id:             (str) can't be null, foreign key to users.id
+        name:                (str) can't be null
+        description:         (str) can be null
+        number_rooms:        (int) can't be null, default value: 0
+        number_bathrooms:    (int) can't be null, default value: 0
+        max_guest:           (int) can't be null, default value: 0
+        price_by_night:      (int) can't be null, default value: 0
+        latitude:            (float) can be null, default value: 0.0
+        longitude:           (float) can be null, default value: 0.0
         amenity_ids:         (list) will be Amenity.id
     """
-    city_id = ""
-    user_id = ""
-    name = ""
-    description = ""
-    number_rooms = 0
-    number_bathrooms = 0
-    max_guest = 0
-    price_by_night = 0
-    latitude = 0.0
-    longitude = 0.0
+    __tablename__ = "places"
+    city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
+    user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+    name = Column(String(128), nullable=False)
+    description = Column(String(1024), nullable=True)
+    number_rooms = Column(Integer, nullable=False, default=0)
+    number_bathrooms = Column(Integer, nullable=False, default=0)
+    max_guest = Column(Integer, nullable=False, default=0)
+    price_by_night = Column(Integer, nullable=False, default=0)
+    latitude = Column(Float, nullable=True, default=0.0)
+    longitude = Column(Float, nullable=True, default=0.0)
     amenity_ids = []
+
+    # Establishing relationships
+    reviews = relationship("Review", backref="place")
+    amenities = relationship("Amenity", secondary=place_amenity, viewonly=False) 
+
--- models/user.py
+++ models/user.py
@@ -1,17 +1,24 @@
 #!/usr/bin/python3
 
-
-"""user class, subclass of BaseModel.
-"""
-
+"""Defines a User class, a subclass of BaseModel."""
 from models.base_model import BaseModel
-import json
+from sqlalchemy import Column, String
 
 
 class User(BaseModel):
-    '''subclass of BaseModel class.'''
+    """
+    A subclass of BaseModel class.
+    Public class attributes:
+        __tablename__: (str) represents the table name, users
+        email:          (str) can't be null
+        password:       (str) can't be null
+        first_name:     (str) can be null
+        last_name:      (str) can be null
+    """
+    __tablename__ = "users"
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128), nullable=True)
+    last_name = Column(String(128), nullable=True)
 
-    email = ""
-    password = ""
-    first_name = ""
-    last_name = ""
+
